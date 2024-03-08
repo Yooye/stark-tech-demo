9a48ebbc3c30a46b968ab294ad2732f5f71258c1
Allen <1944130693@qq.com>
2024-03-08 15:28:11 +0800

    fixed lint warning

--- src/layout/components/NavBar.tsx
+++ src/layout/components/NavBar.tsx
@@ -32,7 +32,7 @@ const SearchIconWrapper = styled("div")(({ theme }) => ({
   justifyContent: "center",
 }));
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
+const StyledInputBase = styled(InputBase)(() => ({
   width: "100%",
   fontSize: "14px",
   paddingLeft: "10px",
